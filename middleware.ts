f4b3b7b777a12e529b056470d706abbfed5d8b5b
Nati Peretz <nati.peretz1985@gmail.com>
2024-01-28 08:31:13 +0200

    fixed public route in middleware

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 export default authMiddleware({
-  publicRoutes: ["/", "hotel-details/:id", "/api/uploadthing"],
+  publicRoutes: ["/", "/hotel-details/:id", "/api/uploadthing"],
 });
 
 export const config = {
