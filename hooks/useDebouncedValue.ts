7c1f4ec09b1ccac9201a2569c0e3b65fae59b841
Nati Peretz <nati.peretz1985@gmail.com>
2024-01-31 15:06:40 +0200

    added location search

--- hooks/useDebouncedValue.ts
+++ hooks/useDebouncedValue.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 export function useDebouncedValue<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 300);
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
     return () => {
       clearTimeout(timer);
     };
